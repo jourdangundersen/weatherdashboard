736383d1d3e3916384575cb6d738a4bf107c1476
Jourdan Gundersen <jourdancgundersen@gmail.com>
2021-01-20 17:35:44 -0600

    added 5 day weather forecast to script.js file

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@ $("#searchButton").on("click", function(event){
     var cityName = $("#cityName").val();
     console.log(cityName,"City name entered");
     getCurrentForcast(cityName);
+    fiveDayForecast(cityName);
 });
 function getCurrentForcast (cityName){
     $.ajax({
@@ -26,12 +27,27 @@ function getCurrentForcast (cityName){
 }
 function fiveDayForecast (cityName){
     $.ajax({
-        url:`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=29e66bd203ea7e7fb543299155a97662&units=imperial`,
+        url:`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=29e66bd203ea7e7fb543299155a97662&units=imperial`,
         method:"GET"
 
     })
     .then(function(apiData){
         console.log(apiData);
+        apiData = apiData.list
+        console.log(apiData)
+        var htmlString = ""
+        for (let i =0;i<apiData.length;i=i+8){
+             htmlString += `
+                <div class="card">
+                <h3>City: ${cityName}</h3>
+                <h6>Temp: ${apiData[i].main.temp}<span><img src="http://openweathermap.org/img/wn/${apiData[i].weather[0].icon}@2x.png" /><span></h6>
+                <p>Wind Speed: ${apiData[i].wind.speed}</p>
+                <p>Humidity: ${apiData[i].main.humidity}</p>
+                </div>
+             `
+
+        }
+        $("#fiveDayForecast").html(htmlString);
         // $("#forecast").html(`
         // <div class="container-fluid">
         // <h3>City: ${cityName}</h3>
