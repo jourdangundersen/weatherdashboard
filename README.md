d7063943259906ddb781ceb0f7e2ecd48e969ec0
Jourdan Gundersen <jourdancgundersen@gmail.com>
2021-01-23 22:59:40 -0600

    added comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # weatherdashboard
+
+# purpose
+- the purpose of this program is to allow the user to input a city of their desire and have a uv index, 5 day forecast, and current forecast for that city be pulled from an api and appear on the screen
+- the program also must display the last inputted city's data that was stored in localStorage
+
+# index.html walkthrough
+- user input defining the cityName variable used throughout the script.js file
+- div tag displaying the uv index
+- div tag displaying the five day forecast
+- div tag displaying the results of the previous inputted city that was stored in localStorage
+
+# script.js walkthrough
+- function displaying the previously searched city's data
+- created cityName variable defined by user input
+- calling the getCurrentForecast and fiveDayForecast functions in order to display their data
+- storing cityName in localStorage
+- pulling from api
+- displaying the data from the current forecast
+- creating variables for lat and lon for the uv index
+- calling uvIndex function
+- pulling from the api to get information for the fiveDayForecast funciton
+- pulling from api for uv index data
+- function to retrieve data from localStorage
+
+# style.css walthrough
+- made the heading bigger and italicized
+- made the search button pink
+- made the font asking for user input larger
+
+# working link 
+# https://jourdangundersen.github.io/weatherdashboard/
+
+# screenshot
+![](assets/images/weather-dashboard-screenshot.png)
--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,48 @@
 $(document).ready(function() {
+    // calling function to display the last searched city's data
     displayLastSearched();
 console.log("JS file loaded");
 $(document).on("click", "#searchButton", function(event){
     event.preventDefault();
+    // creating cityName variable from user input
     var cityName = $("#cityName").val();
     console.log(cityName,"city name entered");
+    // calling the getCurrentForecast and fiveDayForecast functions to display their data
     getCurrentForcast(cityName);
     fiveDayForecast(cityName);
+    // storing the cityName in localStorage
     localStorage.setItem("lastSearched", JSON.stringify(cityName));
 });
 function getCurrentForcast (cityName){
     $.ajax({
+        // pulling from api
         url:`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=29e66bd203ea7e7fb543299155a97662&units=imperial`,
         method:"GET"
         
     })
+    // displaying the data from the current forecast
     .then(function(apiData){
         console.log(apiData);
         $("#forecast").html(`
         <div class="container-fluid">
         <h3>City: ${cityName}</h3>
-        <h6>Temp: ${apiData.main.temp}<span><img src="http://openweathermap.org/img/wn/${apiData.weather[0].icon}@2x.png" /><span></h6>
+        <h6>Temp: ${apiData.main.temp}<span><img src="https://openweathermap.org/img/wn/${apiData.weather[0].icon}@2x.png" /><span></h6>
         <p>Wind Speed: ${apiData.wind.speed}</p>
         <p>Humidity: ${apiData.main.humidity}</p>
 
 
         `);
+        // creating lat and lon variables
         var lat = apiData.coord.lat;
         var lon = apiData.coord.lon;
         console.log(lat, lon);
+        // calling uvIndex function
         uvIndex(lat, lon);
     });
 }
 function fiveDayForecast (cityName){
     $.ajax({
+        // pulling from api for the five day forecast
         url:`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=29e66bd203ea7e7fb543299155a97662&units=imperial`,
         method:"GET"
 
@@ -43,11 +52,12 @@ function fiveDayForecast (cityName){
         apiData = apiData.list;
         console.log(apiData);
         var htmlString = "";
+        // displaying the data
         for (let i =0;i<apiData.length;i=i+8){
              htmlString += `
                 <div class="card">
                 <p>Date: ${apiData[i].dt_txt}</p>
-                <h6>Temp: ${apiData[i].main.temp}<span><img src="http://openweathermap.org/img/wn/${apiData[i].weather[0].icon}@2x.png" /><span></h6>
+                <h6>Temp: ${apiData[i].main.temp}<span><img src="https://openweathermap.org/img/wn/${apiData[i].weather[0].icon}@2x.png" /><span></h6>
                 <p>Wind Speed: ${apiData[i].wind.speed}</p>
                 <p>Humidity: ${apiData[i].main.humidity}</p>
                 </div>
@@ -70,6 +80,7 @@ function uvIndex (lat, lon){
     // http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
     // http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
     $.ajax({
+        // pulling from api for uv index data
         url:`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=29e66bd203ea7e7fb543299155a97662`,
         method:"GET"
 
@@ -80,6 +91,7 @@ function uvIndex (lat, lon){
     });
 
 }
+// function to retrieve data from localStorage
 function displayLastSearched (){
     var lastSearchedCity = JSON.parse(localStorage.getItem("lastSearched"));
     var data = "";
